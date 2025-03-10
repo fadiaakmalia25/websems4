7d81228712b854b6ab8c096215a623ff1dea7183
Fadia Akmalia <fadiaakmalia25@gmail.com>
2025-03-09 23:00:26 -0700

    membuat prodi

--- index.php
+++ index.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<, initial-scale=1.0">
-    <title>Document</title>
+    <title>SIMPADUPOLIBAN</title>
 </head>
+
+
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5" >
+        <head>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+        </head>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323008</td>
+                <td>Fadia Akmalia</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323032</td>
+                <td>Leni Rahmah</td>
+            </tr>  
+        </body> 
+    </table>  
+
 </body>
 </html>
